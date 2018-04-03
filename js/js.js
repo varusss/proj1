7cf9f7717ad241a24472a805d485585bda6ba06e
unknown <oanhtran901@gmail.com>
2018-04-02 19:47:43 -0700

    add more stuff

--- js/js.js
+++ js/js.js
@@ -1,26 +1,37 @@
 var slides = document.getElementsByClassName("pic"),
-    current = 0;
+    current = 1;
 
 function nextSlide() {
     slideShows(current += 1);
+    if (current > 2) current = 0;
 }
 
 function prevSlide() {
     slideShows(current -= 1);
+    if (current <= 0) current = slides.length;
 }
+slideShows(current);
 
 function slideShows(current) {
-    var i;
-    if (current > slides.length - 1) {
-        current = 0;
+
+    switch (current) {
+        case 0:
+        case 3:
+            slides[0].style.display = "block";
+            slides[1].style.display = "none";
+            slides[2].style.display = "none";
+            break;
+        case 1:
+            slides[0].style.display = "none";
+            slides[1].style.display = "block";
+            slides[2].style.display = "none";
+            break;
+        case 2:
+            slides[0].style.display = "none";
+            slides[1].style.display = "none";
+            slides[2].style.display = "block";
+            break;
     }
-    if (current < 0) {
-        current = 0;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[current].style.display = "block";
 }
 
 /*Scroll to the end, smooth scroll */
@@ -35,45 +46,41 @@ function down() {
 
 function changeNav() {
     var nav = document.getElementById("nav");
+    var w = window.innerWidth;
+    var h = window.innerHeight;
     window.onscroll = function () {
-        if (window.scrollY > 400) {
-            nav.setAttribute("style", "background:linear-gradient(-90deg, #ffffff, #ffceaf, #ffffff);");
-        } else {
-            nav.setAttribute("style", "background: linear-gradient(-90deg, #ffffff, #86bcdd, #ffffff);");
-        }
+        if (w > 700) {
+            if (window.scrollY > h) {
+                nav.setAttribute("style", "background:linear-gradient(-90deg, #ffffff, #ffceaf, #ffffff); box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);");
+            } else {
+                nav.setAttribute("style", "background: transparent");
+            }
+        } 
+
     };
 };
 
 function validate() {
     var name = document.getElementById("name").value,
         email = document.getElementById("email").value,
-        //msm = document.getElementById("msm").value,
         respond = document.getElementById("invalide"),
         /*Regular Expression*/
         reg = new RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", "g"),
         CheckEmail = email.match(reg);
 
-    /*Check if @ is there */
+    /*Check email format */
     reg = reg.test(CheckEmail);
-    //console.log(typeof msm);
 
     /*Form validation*/
-    if (isNaN(name) === false && reg === false) {
-        respond.innerHTML = "<p>*Name not valid <br> *Email not valid</p>";
+    if (isNaN(name) === false) {
+        respond.innerHTML = "<p>*Name not valid</p>";
     }
     if (isNaN(name) === false) {
         respond.innerHTML = "<p> *Name is not valid </p>"
     }
     if (reg === false) {
         respond.innerHTML = "<p> *Invalid Email</p>"
-    }
-    if (isNaN(name) === true && reg === true) {
-        document.getElementById("form").submit();
-    }
-    //    if(msm === null){
-    //        respond.innerHTML ="<p>Blank Message</p>"
-    //    }
-    else {
+    } else {
         console.log("Reset");
     }
 }
